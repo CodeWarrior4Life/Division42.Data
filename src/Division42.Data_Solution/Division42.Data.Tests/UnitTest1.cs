a7c28141c5bb6307cc2a2410495a44dc8ebe34c7
Robert Seder <RobSeder@live.com>
2014-07-07 17:24:30 -0400

    Added unit test project.

---
+++ src/Division42.Data_Solution/Division42.Data.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Division42.Data.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
